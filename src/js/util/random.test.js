c03c174254d9b548ba2281ab9953c7ed909ee644
Lars Thorup <lars@zealake.com>
2012-10-30 20:19:07 -0700

    lint: qunit

--- src/js/util/random.test.js
+++ src/js/util/random.test.js
@@ -1,9 +1,8 @@
-/*global require,sinon,QUnit*/
+/*jslint undef:true, sloppy:true*/ // Note: to avoid having to write QUnit.module, etc
 require([
     "util/random"
 ], function () {
-    "use strict";
-    QUnit.module("util/random", {
+    module("util/random", {
         setup: function () {
             sinon.stub(Math, "random").returns(0.85);
         },
@@ -12,9 +11,9 @@ require([
         }
     });
 
-    QUnit.test("randomBelow", function () {
-        QUnit.equal(Math.randomBelow(6), 5, "6");
-        QUnit.equal(Math.randomBelow(1), 0, "1");
-        QUnit.throws(function () { Math.randomBelow(0); }, "invalid argument: 0", "0");
+    test("randomBelow", function () {
+        equal(Math.randomBelow(6), 5, "6");
+        equal(Math.randomBelow(1), 0, "1");
+        throws(function () { Math.randomBelow(0); }, "invalid argument: 0", "0");
     });
 });
