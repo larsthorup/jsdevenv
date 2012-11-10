fd4c238aaf2da347cac94b5a69003f6a5a96840f
Lars Thorup <lars@zealake.com>
2012-11-10 14:16:40 -0800

    fixing broken build (coverage)

--- src/js/util/random.js
+++ src/js/util/random.js
@@ -3,8 +3,5 @@ Math.randomBelow = function (n) {
     if (n < 1) {
         throw 'invalid argument: ' + n;
     }
-    if (n > 1000) {
-        throw 'invalid argument: ' + n;
-    }
     return Math.floor(Math.random() * n);
 };
